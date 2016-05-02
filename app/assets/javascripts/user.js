5e375b99a6166e747797c354dbb56d51bbd12fff
joeylehr <joe.c.lehr@gmail.com>
2016-05-01 23:25:01 -0400

    adds shia labeouf

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -47,19 +47,20 @@ $(document).on('ready', function(){
   });
 })
 
-// function autoPlayYouTubeModal(){
-//   var trigger = $("body").find('[data-toggle="modal"]');
-//   trigger.click(function() {
-//     var theModal = $(this).data( "target" ),
-//     videoSRC = $(this).attr( "data-theVideo" ), 
-//     videoSRCauto = videoSRC+"?autoplay=1" ;
-//     $(theModal+' iframe').attr('src', videoSRCauto);
-//     $(theModal+' button.close').click(function () {
-//         $(theModal+' iframe').attr('src', videoSRC);
-//     });   
-//   });
-// }
+// SHIA LABEOUF VIDEO (not my code)
+function autoPlayYouTubeModal(){
+  var trigger = $("body").find('[data-toggle="modal"]');
+  trigger.click(function() {
+    var theModal = $(this).data( "target" ),
+    videoSRC = $(this).attr( "data-theVideo" ), 
+    videoSRCauto = videoSRC+"?autoplay=1" ;
+    $(theModal+' iframe').attr('src', videoSRCauto);
+    $(theModal+' button.close').click(function () {
+        $(theModal+' iframe').attr('src', videoSRC);
+    });   
+  });
+}
 
-// $(document).ready(function(){
-//   autoPlayYouTubeModal();
-// });
+$(document).ready(function(){
+  autoPlayYouTubeModal();
+});
